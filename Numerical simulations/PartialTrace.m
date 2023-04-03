c9421f65bd341e49e8adc504e5077309c05d96e8
Sjd-Hz <128447521+Sjd-Hz@users.noreply.github.com>
2023-04-03 10:47:35 +0800

    Update PartialTrace.m

--- Numerical simulations/PartialTrace.m
+++ Numerical simulations/PartialTrace.m
@@ -1,13 +1,19 @@
 function [result] = PartialTrace(n1, n2)
-%PartialTrace4 此处显示有关此函数的摘要
-%   此处显示详细说明
+%PartialTrace: this is a function of the partial trace operation on the quantum subsystem
+%Input: n1 is the density matrix of the whole quantum system
+%       n2 is an array which contains the sequence numbers that make up the quantum subsystem
+%Output: result is the density matrix after the partial trace operation
+%Example: n1 is a 8*8 density matrix, n2=[1,3], this respresents that the
+%partial trace on the quantum subsystem consisting of the first and third
+%quantum bit
+
 result=0;
 temp=1;
 I=eye(2);
 s0=[1;0];
 s1=[0;1];
 base={};
-%子系统n2的基底获取
+%obtain the basis corresponding to the subsystem (n2)
 for i=1:log2(size(n1,1))
     if ~ismember(i,n2)
         if size(base,2)==0
@@ -34,7 +40,7 @@ for i=1:log2(size(n1,1))
         end    
     end    
 end
-%偏迹运算
+%partial trace operation
 for i=1:size(base,2)
     result=result+base{i}'*n1*base{i};
 end
